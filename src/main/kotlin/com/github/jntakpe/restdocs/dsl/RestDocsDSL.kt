1910a9393a71893367de752b3ac3ed22b2425978
jntakpe <jntakpe@gmail.com>
2019-01-07 14:47:21 +0100

    Support arrays

--- src/main/kotlin/com/github/jntakpe/restdocs/dsl/RestDocsDSL.kt
+++ src/main/kotlin/com/github/jntakpe/restdocs/dsl/RestDocsDSL.kt
@@ -2,10 +2,13 @@ package com.github.jntakpe.restdocs.dsl
 
 import org.springframework.restdocs.payload.FieldDescriptor
 import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.JsonFieldType.ARRAY
 import org.springframework.restdocs.payload.JsonFieldType.BOOLEAN
+import org.springframework.restdocs.payload.JsonFieldType.NULL
 import org.springframework.restdocs.payload.JsonFieldType.NUMBER
 import org.springframework.restdocs.payload.JsonFieldType.OBJECT
 import org.springframework.restdocs.payload.JsonFieldType.STRING
+import org.springframework.restdocs.payload.JsonFieldType.VARIES
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 
 @DslMarker
@@ -24,31 +27,61 @@ internal interface Field {
 }
 
 @RestDocsMarker
-abstract class Nested(private val path: String) {
+abstract class Nested(protected val basePath: String) {
 
-    internal val children = arrayListOf<FieldDescriptor>()
+    val fields: MutableList<FieldDescriptor> = arrayListOf()
+
+    fun array(name: String, description: String, optional: Boolean = false, block: Array.() -> Unit): Array {
+        val arrayName = "$name[]"
+        val fullPath = if (basePath.isEmpty()) "$arrayName." else "$basePath$arrayName."
+        return nested(Array(arrayName, description, optional, fullPath), block)
+    }
 
     fun boolean(name: String, description: String, optional: Boolean = false) = field(Bool(name, description, optional))
+
+    fun nil(name: String, description: String, optional: Boolean = false) = field(Nil(name, description, optional))
 
     fun number(name: String, description: String, optional: Boolean = false) = field(Number(name, description, optional))
 
     fun string(name: String, description: String, optional: Boolean = false) = field(Text(name, description, optional))
 
     fun json(name: String, description: String, optional: Boolean = false, block: Json.() -> Unit): Json {
-        val fullPath = if (path.isEmpty()) "$name." else "$path$name."
+        val fullPath = if (basePath.isEmpty()) "$name." else "$basePath$name."
         return nested(Json(name, description, optional, fullPath), block)
     }
 
-    private fun <T : Field> field(field: T) = also { children.add(field.build(path)) }
+    fun varies(name: String, description: String, optional: Boolean = false) = field(Varies(name, description, optional))
+
+    operator fun MutableCollection<in FieldDescriptor>.plusAssign(elements: Iterable<FieldDescriptor>) {
+        addAll(elements.map { it.rebase() })
+    }
+
+    private fun <T : Field> field(field: T): T = field.also { fields.add(field.build(basePath)) }
 
     private fun <T> nested(field: T, block: T.() -> Unit) where T : Nested, T : Field = field.also {
         it.block()
-        children.add(field.build(path))
-        children.addAll(field.children)
+        fields.add(field.build(basePath))
+        fields.addAll(field.fields)
     }
+
+    private fun FieldDescriptor.rebase() = fieldWithPath("$basePath$path").type(type).description(description).opt()
+
+    private fun FieldDescriptor.opt() = takeIf { isOptional }?.optional() ?: this
 }
 
 class Root : Nested("")
+
+class List(description: String) : Nested("[].") {
+    init {
+        fields.add(fieldWithPath("[]").description(description).type(ARRAY))
+    }
+}
+
+class Array(override val name: String, override val description: String, override val optional: Boolean, path: String)
+    : Nested(path), Field {
+
+    override val type = ARRAY
+}
 
 class Bool(override val name: String, override val description: String, override val optional: Boolean) : Field {
     override val type = BOOLEAN
@@ -60,6 +93,10 @@ class Json(override val name: String, override val description: String, override
     override val type = OBJECT
 }
 
+class Nil(override val name: String, override val description: String, override val optional: Boolean) : Field {
+    override val type = NULL
+}
+
 class Number(override val name: String, override val description: String, override val optional: Boolean) : Field {
     override val type = NUMBER
 }
@@ -68,6 +105,11 @@ class Text(override val name: String, override val description: String, override
     override val type = STRING
 }
 
+class Varies(override val name: String, override val description: String, override val optional: Boolean) : Field {
+    override val type = VARIES
+}
+
 object JsonDescriptor {
-    fun root(init: Root.() -> Unit) = Root().apply { init() }.children
+    fun root(init: Root.() -> Unit) = Root().apply { init() }.fields
+    fun list(description: String, init: List.() -> Unit) = List(description).apply { init() }.fields
 }
