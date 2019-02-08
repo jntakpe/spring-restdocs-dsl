520efe956acb9e427564d61ff971ddf9e2dc31c0
jntakpe <jntakpe@gmail.com>
2019-02-08 19:53:11 +0100

    Update Kotlin to 1.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.3.21"
     id("org.jetbrains.dokka") version "0.9.17"
     `build-scan`
     `maven-publish`
